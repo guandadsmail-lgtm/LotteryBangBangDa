0f913314e80e5c8567b43c24c5a34e66d4504564
JWGroup <guandadsmail@gmail.com>
2026-01-16 21:56:30 +0800

    Initial Commit

---
+++ 彩票棒棒哒/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  彩票棒棒哒
+//
+//  Created by Guandad on 2026/1/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
